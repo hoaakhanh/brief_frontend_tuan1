a58afc374b03bf7c3555e58fe6421053c6229ce6
HoaKhanh <hoaakhanh07@gmail.com>
2026-07-23 11:49:28 +0700

    Updated

--- js/script.js
+++ js/script.js
@@ -109,6 +109,11 @@ function createAnimeCard(anime) {
     const genres = anime.genres.map(genre => genre.name); /* voi moi genre lay thuoc tinh name */
     genre.textContent = genres.join(" • ");
 
+    //Detail Button
+    const detailButton = document.createElement("button");
+    detailButton.classList.add("detail-btn");
+    detailButton.textContent = "More Details";
+
     // Favorite Button
     const favoriteButton = document.createElement("button");
     favoriteButton.classList.add("favorite-btn");
@@ -134,56 +139,74 @@ function createAnimeCard(anime) {
         }
     });
 
-
-    //Detail Button
-    const detailButton = document.createElement("button");
-    detailButton.classList.add("detail-btn");
-    detailButton.textContent = "More Details";
-
     // Dua tat ca vao card 
     card.appendChild(image);
     card.appendChild(title);
     card.appendChild(rating);
     card.appendChild(genre);
-    card.appendChild(favoriteButton);
     card.appendChild(detailButton);
-
+    card.appendChild(favoriteButton);
+    
     return card;
     
 };
 
 
-
-
 /* API */
+const animeList = document.querySelector("#anime-list");
+
 fetch("https://api.jikan.moe/v4/top/anime")
     .then(response => response.json())
     .then(result => {
         result.data.forEach(anime => { /* lay tu kho ttin */
         
-            const animeLIst = document.querySelector("#anime-list");
             const card = createAnimeCard(anime);
 
             // Dua cac card vao list
-            animeLIst.appendChild(card); /* dua card vao movie-grid */
+            animeList.appendChild(card); /* dua card vao movie-grid */
             
-});
+        });
+    });
 
 // Search Input
 const searchInput = document.querySelector(".search-input");
 const searchBtn = document.querySelector("#search-btn");
 
+const searchDiv = document.querySelector(".search-result-div");
+const closeSearchBtn = document.querySelector("#close-search");
+
+const animeSearch = document.querySelector("#search-result")
+
 searchBtn.addEventListener("click", function() {
     const keyword = searchInput.value.trim();
+
     if (!keyword) {
         return;
     }
 
+    searchDiv.classList.add("active");
+
     fetch(`https://api.jikan.moe/v4/anime?q=${keyword}`)
         .then(response => response.json())
         .then(result => {
-            console.log(result);
-        })
-});
 
-    });
+            // Xóa Card cũ
+            animeSearch.innerHTML = "";
+
+            // Hiển thị kết quả Search
+            result.data.forEach(anime => {
+
+                const card = createAnimeCard(anime);
+
+                animeSearch.appendChild(card);
+
+            });
+            
+        })
+
+});
+closeSearchBtn.addEventListener("click", function() {
+
+    searchDiv.classList.remove("active");
+
+});
